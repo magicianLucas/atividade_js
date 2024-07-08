35fb2bf4789e6ad37bc820e1419c2a219576a297
LUCAS TELES DOS SANTOS <LUCAS6263@DIGITALCOLLEGE.LOCAL>
2024-07-08 13:18:10 -0300

    atualização

---
+++ metodosAgregadores.js
@@ -0,0 +1,9 @@
+let frutas = ['maça', 'uva', 'banana']
+let numeros = [1, 2, 3]
+let retorno = frutas.forEach
+((FRUTA)=>console.log(FRUTA.toUpperCase()))
+console.log(frutas)
+
+let numerosDobro = numeros.map ((num) => {console.log(num)
+    return num*2
+})
